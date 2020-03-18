f7912182d125061056abb863fbbc3cb9c0b7ce4f
Linh Vu Hai Nguyen <linh.nguyen.nvhl@gmail.com>
2020-03-17 20:46:38 -0400

    made function to generate random string

--- express_server.js
+++ express_server.js
@@ -2,16 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port for vagrant environment
 const bodyParser = require("body-parser");
+const numChar = 6; //number of characters in short url
+
+function generateRandomString(numChar) {
+  let string = '';
+  const chararcters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (let i = numChar; i > 0; --i) {
+      string += chararcters[Math.floor(Math.random() * chararcters.length)];
+    }
+    return string;
+};
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('partial', '/partial/_header');
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+
 
 app.get("/", (req, res) => {
   res.send('Hello!');
@@ -20,6 +32,11 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls.json", (req, res) => {
